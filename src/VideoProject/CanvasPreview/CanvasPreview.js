ab455691a13bdec2805a8e57c775b9a397a0cb83
chnbohwr <chnbohwr@gmail.com>
2018-07-09 15:52:08 +0800

    arrangement state

--- src/VideoProject/CanvasPreview/CanvasPreview.js
+++ src/VideoProject/CanvasPreview/CanvasPreview.js
@@ -2,23 +2,26 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Rnd from 'react-rnd';
 
-const totalWidthHeight = canvasList => canvasList.reduce((acc, val) => {
-  val.coordinate_out.forEach((coord) => {
-    acc = {
-      minX: (coord.x < acc.minX) ? coord.x : acc.minX,
-      minY: (coord.y < acc.minY) ? coord.y : acc.minY,
-      maxX: (coord.x > acc.maxX) ? coord.x : acc.maxX,
-      maxY: (coord.y > acc.maxY) ? coord.y : acc.maxY,
-    };
-  });
-  return acc;
-},
-  {
-    minX: Infinity,
-    minY: Infinity,
-    maxX: 0,
-    maxY: 0
-  });
+const totalWidthHeight = (canvasList) => {
+  const xy = canvasList.reduce((acc, val) => {
+    val.coordinate_out.forEach((coord) => {
+      acc = {
+        minX: (coord.x < acc.minX) ? coord.x : acc.minX,
+        minY: (coord.y < acc.minY) ? coord.y : acc.minY,
+        maxX: (coord.x > acc.maxX) ? coord.x : acc.maxX,
+        maxY: (coord.y > acc.maxY) ? coord.y : acc.maxY,
+      };
+    });
+    return acc;
+  },
+    {
+      minX: Infinity,
+      minY: Infinity,
+      maxX: 0,
+      maxY: 0
+    });
+  return { width: xy.maxX - xy.minX, height: xy.maxY - xy.minY };
+};
 
 export default class CanvasPreview extends PureComponent {
   static propTypes = {
@@ -33,12 +36,7 @@ export default class CanvasPreview extends PureComponent {
 
   state = {
     position: { x: 0, y: 0, },
-    size: {
-      width: this.cd.maxX - this.cd.minX,
-      height: this.cd.maxY - this.cd.minY
-    },
     ratio: 1,
-    viewBox: `0 0 ${this.cd.maxX - this.cd.minX} ${this.cd.maxY - this.cd.minY}`
   }
 
   onDragStop = (e, d) => {
@@ -47,21 +45,14 @@ export default class CanvasPreview extends PureComponent {
 
   onResize = (e, direction, ref) => {
     const width = parseInt(ref.style.width, 10);
-    const height = parseInt(ref.style.height, 10);
-    const originalWidth = this.cd.maxX - this.cd.minX;
-    const originalHeight = this.cd.maxY - this.cd.minY;
-    const ratio = width / originalWidth;
-    this.setState({
-      ratio,
-      size: { width, height }
-    });
+    const ratio = width / this.cd.width;
+    this.setState({ ratio });
   }
 
   render() {
     const { canvasList } = this.props;
-    const {
-      size, position, viewBox, ratio,
-    } = this.state;
+    const { position, ratio, } = this.state;
+    const size = { width: this.cd.width * ratio, height: this.cd.height * ratio };
     return (
       <Rnd
         style={{ border: '1px solid yellow' }}
@@ -70,17 +61,17 @@ export default class CanvasPreview extends PureComponent {
         onResize={this.onResize}
         lockAspectRatio
         onDragStop={this.onDragStop}>
-        <svg width={size.width} height={size.height} viewBox={viewBox}>
+        <svg width={size.width} height={size.height}>
           {
             canvasList.map(data => (
               <path
                 stroke="yellow"
                 fill="transparent"
                 d={`
-              M${data.coordinate_in[0].x} ${data.coordinate_in[0].y}
-              L${data.coordinate_in[1].x} ${data.coordinate_in[1].y}
-              L${data.coordinate_in[2].x} ${data.coordinate_in[2].y}
-              L${data.coordinate_in[3].x} ${data.coordinate_in[3].y}
+              M${data.coordinate_in[0].x * ratio} ${data.coordinate_in[0].y * ratio}
+              L${data.coordinate_in[1].x * ratio} ${data.coordinate_in[1].y * ratio}
+              L${data.coordinate_in[2].x * ratio} ${data.coordinate_in[2].y * ratio}
+              L${data.coordinate_in[3].x * ratio} ${data.coordinate_in[3].y * ratio}
               Z
               `} />
             ))
